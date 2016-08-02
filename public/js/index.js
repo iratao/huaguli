b3b3448e552e164c11fc57333beabdc18f36d835
iratao <iratao@gmail.com>
2016-08-02 23:04:22 +0800

    error dialog

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,22 @@ $(function() {
     });
 
     $("#submit_btn").click(function(event) {
-        $('#loading').removeClass('invisible');
     	var selectedItem = $(".flower_item.active").attr("data-value");
     	var seatNum = $("#seat_number").val();
+        var phoneNum = $("#phone_number").val();
+        if (!selectedItem) {
+            alert('please select an item');
+            return;
+        }
+        if (!seatNum) {
+            alert('please give your seat number');
+            return;
+        }
+        if (!phoneNum) {
+            alert('please give your phone number');
+            return;
+        }
+        $('#loading').removeClass('invisible');
     	$.ajax({
             type: 'POST',
             url: '/api/sendmail',
@@ -26,6 +39,14 @@ $(function() {
         });
     });
 
+    $("#success_ok_btn").click(function(){
+        $('#order_success').addClass('invisible');
+    });
+
+    $("#fail_ok_btn").click(function(){
+        $('#order_fail').addClass('invisible');
+    });
+
     function submitSuccess(data) {
         $('#loading').addClass('invisible');
         console.log("submit success");
@@ -35,5 +56,6 @@ $(function() {
     function submitError(xhr, status, error) {
         $('#loading').addClass('invisible');
         console.log("submit error: " + error);
+        $('#order_fail').removeClass('invisible');
     }
 });
